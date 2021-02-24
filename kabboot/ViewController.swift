d01b83b41b20d7016812275d52a54ae923e20369
Usama Fouad <usamaeltmsah@gmail.com>
2021-02-24 20:57:01 +0200

    Initial Commit

---
+++ kabboot/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  kabboot
+//
+//  Created by Usama Fouad on 23/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var registerButton: UIButton!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var userNameView: UIView!
+    @IBOutlet var phoneNumberView: UIView!
+    @IBOutlet var emailView: UIView!
+    @IBOutlet var passwordView: UIView!
+    @IBOutlet var confirmPasswordView: UIView!
+    @IBOutlet var register: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let rowsViews = [userNameView, phoneNumberView, emailView, passwordView, confirmPasswordView, register]
+           
+        for view in rowsViews {
+            view?.layer.cornerRadius = 20
+        }
+    }
+
+    @IBAction func registerClicked(_ sender: Any) {
+        registerButton.isSelected = true
+        loginButton.isSelected = false
+    }
+    @IBAction func loginClicked(_ sender: Any) {
+        registerButton.isSelected = false
+        loginButton.isSelected = true
+    }
+    
+    @IBAction func register(_ sender: Any) {
+    }
+}
+
+extension UIColor {
+    convenience init(rgb: Int, alpha: CGFloat) {
+        self.init(
+            red: CGFloat((rgb >> 16) & 0xFF),
+            green: CGFloat((rgb >> 8) & 0xFF),
+            blue: CGFloat(rgb & 0xFF),
+            alpha: alpha
+        )
+    }
+}
